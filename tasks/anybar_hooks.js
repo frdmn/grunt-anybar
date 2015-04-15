6ee1e56fd4333108dc0c0124c4f7120d3c1c106f
Jonas Friedmann <j@frd.mn>
2015-04-15 21:54:09 +0100

    Remove unused stuff

--- tasks/anybar_hooks.js
+++ tasks/anybar_hooks.js
@@ -10,13 +10,8 @@
 
 module.exports = function gruntTask(grunt) {
 
-  var guessProjectName = require('../lib/util/guessProjectName');
-
   // All of these settings are customizable via notify_hooks
   var defaults = {
-    enabled: true,
-    max_jshint_notifications: 5,
-    success: false,
     port: 1738,
     status: ''
   };
