209f12c180607ce460e578518e298a7f60aae687
Jonas Friedmann <j@frd.mn>
2015-04-15 21:53:50 +0100

    Add node-anybar dependency

--- tasks/anybar.js
+++ tasks/anybar.js
@@ -13,38 +13,21 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
+  var anybar = require('anybar');
+
+  var defaults = {
+    port:       1738,
+    status:     ''
+  };
+
   grunt.registerMultiTask('anybar', 'Plugin to show the current build status of Grunt in the AnyBar menu bar.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var options = this.options(defaults);
+    var done = this.async();
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+    if (options.status) {
+      anybar(options.status, options, done);
+    } else {
+      done(new Error('AnyBar status is required. Make sure your AnyBar configuration is inside an options object.'));
+    }
   });
-
 };
